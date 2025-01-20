ea22ac8c37db9a893a083474e8d694006f14a794
heartlesspatidar <npdnitin9@gmail.com>
2025-01-20 10:53:04 +0530

    updatee

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Dynamic base path configuration
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.NODE_ENV === 'production' ? '/shopping-client/' : '/', 
+  server: {
+    port: process.env.VITE_PORT || 5173,
+  },
+});
